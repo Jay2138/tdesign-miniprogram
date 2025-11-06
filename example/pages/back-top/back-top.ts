1fb98d15aba1465e963801b4b1c5415273b6b142
yichenhuan <heng_huan@126.com>
2025-11-06 11:20:41 +0800

    feat: 处理冲突

---
+++ example/pages/back-top/back-top.ts
@@ -0,0 +1,22 @@
+Page({
+  data: {
+    type: 'round',
+    scrollTop: 0,
+    rowCol: [{ size: '327rpx', borderRadius: '24rpx' }, 1, { width: '61%' }],
+  },
+
+  onPageScroll(e) {
+    const { scrollTop } = e;
+    this.setData({ scrollTop });
+  },
+
+  onBtnClick(e: any) {
+    const { source: type } = e.currentTarget.dataset;
+
+    this.setData({
+      type,
+    });
+
+    wx.pageScrollTo({ duration: 300, scrollTop: 1000 });
+  },
+});
